cec0a3e97db25f9a76a0e1c3f16c4dd2dbdc2fc4
natrix_dev <discordlenko@gmail.com>
2022-02-17 19:58:48 +0100

    Create git_import.js

---
+++ git_import.js
@@ -0,0 +1,13 @@
+const api = requir("git-desktop-form=gitDEv")
+
+let x = 1
+let c = 2
+let v = 3
+
+api.program.launch()=> 
+console.input("repo name ?")
+let response = input 
+
+response. api .find()=> .then (downloadIt) [form:.7z]
+
+then console.log(x, c, v + "Pogram completed")
